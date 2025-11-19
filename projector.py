e7887d123112ecd1b78c1c76dbe979c94bd35dc6
graptolite <109924401+graptolite@users.noreply.github.com>
2025-11-19 17:27:40 +0000

    Add structure to code.

--- projector.py
+++ projector.py
@@ -23,77 +23,103 @@ from numpy import cos,sin,tan
 import re
 import argparse
 
-# Handle commandline args.
-parser = argparse.ArgumentParser()
-parser.add_argument("-a","--alpha",default=45,type=float,help="alpha angle (angle counterclockwise about the z axis), in degrees")
-parser.add_argument("-g","--gamma",default=np.degrees(np.arctan(np.sqrt(2))),type=float,help="gamma angle (angle counterclockwise about the x axis), in degrees")
-parser.add_argument("-o","--outfile",default="out.svg",type=str,help="path to save projected diagram")
-args = parser.parse_args()
-alpha = np.radians(args.alpha)
-gamma = np.radians(args.gamma)
+class OrthographicProjector():
+    def __init__(self,alpha,gamma):
+        self.alpha = alpha
+        self.gamma = gamma
+        # All anchors are at top left of each side plane of the cuboid (so the y face requires a translation).
+        # Define the overall combined-rotation transformation (see `derivation/derivation.pdf` for details).
+        transform = np.array([[cos(alpha),sin(alpha),0],
+                              [-sin(alpha)*cos(gamma),cos(alpha)*cos(gamma),sin(gamma)]])
+        # Define the transformations for each of the cuboid faces.
+        subtransform = lambda i_a,i_b : np.array([transform[:,i_a],transform[:,i_b]]).T
+        self.transform_x = subtransform(1,2)
+        self.transform_y = subtransform(0,2)
+        self.transform_z = subtransform(0,1)
+        self.t_dict = {"x":self.transform_x,
+                       "y":self.transform_y,
+                       "z":self.transform_z}
+        return
 
-# All anchors are at top left of each side plane of the cuboid (so the y face requires a translation).
-# Define the overall combined-rotation transformation (see `derivation/derivation.pdf` for details).
-transform = np.array([[cos(alpha),sin(alpha),0],
-                      [-sin(alpha)*cos(gamma),cos(alpha)*cos(gamma),sin(gamma)]])
-# Define the transformations for each of the cuboid faces.
-subtransform = lambda i_a,i_b : np.array([transform[:,i_a],transform[:,i_b]]).T
-transform_x = subtransform(1,2)
-transform_y = subtransform(0,2)
-transform_z = subtransform(0,1)
-t_dict = {"x":transform_x,
-          "y":transform_y,
-          "z":transform_z}
+class FaceCollection():
+    def __init__(self,f_xface,f_yface,f_zface):
+        self.f_map = {"x":f_xface,
+                      "y":f_yface,
+                      "z":f_zface}
+        self.svgs = dict()
+        for p,f in self.f_map.items():
+            with open(f) as infile:
+                svg = infile.read()
+            self.svgs[p] = svg
+        return
+    def get_dimensions(self):
+        # Identify the dimensions in each of the svg files (`x.svg`, `y.svg` and `z.svg`).
+        svg_dims = dict()
+        for p,svg in self.svgs.items():
+            svg_props = re.search(r"<svg[\S\s]+?>",svg).group(0)
+            width = float(re.search(r"width=\"([0-9\.]+)",svg_props).group(1))
+            height = float(re.search(r"height=\"([0-9\.]+)",svg_props).group(1))
+            svg_dims[p] = (width,height)
+        return svg_dims
+    def get_toplevel_contents(self):
+        toplevel_content = dict()
+        for p,svg in self.svgs.items():
+            # Construct a toplevel group containing everything under the assumption that the start of the defs is the start of the actual SVG content.
+            defs_start = [m.start() for m in re.finditer(r"<defs[\S\s]+?/>",svg)][0]
+            # Isolate only the toplevel contents without the svg tags.
+            main_group = svg[defs_start:].replace("</svg>","")
+            toplevel_content[p] = main_group
+        return toplevel_content
 
-# Define formatting for combining a transformation for the cuboid face to project it onto the plane with a translation in the projected plane.
-t_str = lambda t,translate : "matrix(" + ",".join(np.concatenate([t.T.flatten(),translate]).astype(str)) + ")"
+def format_t_matrix(t,translate):
+    # Define formatting for combining a transformation for the cuboid face to project it onto the plane with a translation in the projected plane.
+    t_str = "matrix(" + ",".join(np.concatenate([t.T.flatten(),translate]).astype(str)) + ")"
+    return t_str
 
-# Identify the dimensions in each of the svg files (`x.svg`, `y.svg` and `z.svg`).
-svg_dims = dict()
-for p in t_dict:
-    with open(p+".svg") as infile:
-        svg = infile.read()
-    svg_props = re.search(r"<svg[\S\s]+?>",svg).group(0)
-    width = float(re.search(r"width=\"([0-9\.]+)",svg_props).group(1))
-    height = float(re.search(r"height=\"([0-9\.]+)",svg_props).group(1))
-    svg_dims[p] = (width,height)
+def project_svg_collection(alpha,gamma,f_xface,f_yface,f_zface):
+    check_bad_angle = lambda ang : (ang < 0) or (ang > 90)
+    if any([check_bad_angle(ang) for ang in [alpha,gamma]]):
+        raise ValueError("Angles alpha and gamma must both be in the range [0,90] degrees.")
+    proj = OrthographicProjector(alpha,gamma)
+    faces = FaceCollection(f_xface,f_yface,f_zface)
+    svg_dims = faces.get_dimensions()
+    toplevel_content = faces.get_toplevel_contents()
+    # Start output svg with arbitrary header.
+    svg_out = ['<svg height="10mm" width="10mm">']
+    # Interate through each face of the cuboid.
+    for p,t in proj.t_dict.items():
+        # Default to no translation after projection.
+        translate = np.array([0,0])
+        # If the active plane is the y plane, then a transformation does need applying (see `derivation/derivation.pdf` for details).
+        if p=="y":
+            translate = np.dot(proj.transform_x,np.array([svg_dims["x"][0],0]))
+        # Construct toplevel group with the projection transformation matrix.
+        t_str = format_t_matrix(t,translate)
+        toplevel_group_header = '<g transform="%s" id="%s-face">' % (t_str,p)
+        # Store the grouped toplevel contents to the output svg.
+        svg_out.append(toplevel_group_header + toplevel_content[p] + "</g>")
+    # Close the output svg.
+    svg_out.append("</svg>")
+    return "\n".join(svg_out)
 
-# Start output svg with arbitrary header.
-svg_out = ['<svg height="10mm" width="10mm">']
-# Interate through each face of the cuboid.
-for p,t in t_dict.items():
-    with open(p+".svg") as infile:
-        svg = infile.read()
-    # Construct a toplevel group containing everything.
-    defs_start = [m.start() for m in re.finditer(r"<defs[\S\s]+?/>",svg)][0]
-    svg = svg[:defs_start] + "<g>" + svg[defs_start:]
-    svg = svg.replace("</svg>","</g></svg>")
-    # Identify the contents of the toplevel group (including the toplevel group tags).
-    ## This could be simplified...
-    group_starts = [m.start() for m in re.finditer("<g",svg)]
-    group_ends = [m.end() for m in re.finditer("</g>",svg)]
-    main_group = svg[group_starts[0]:group_ends[-1]]
-    main_props = re.search(r"<g[\S\s]*?>",main_group).group(0)
-    # Default to no translation after projection.
-    translate = np.array([0,0])
-    # If the active plane is the y plane, then a transformation does need applying (see `derivation/derivation.pdf` for details).
-    if p=="y":
-        translate = np.dot(transform_x,np.array([svg_dims["x"][0],0]))
-    # Update the toplevel group with the projection transformation.
-    transform_new = "transform=\"%s\"" % t_str(t,translate)
-    main_props_repl = re.sub("transform=\".+?\"",transform_new,main_props)
-    if "transform" not in main_props:
-        main_props_repl = main_props[:-1] + " " + transform_new + ">"
-    main_group = main_group.replace(main_props,main_props_repl)
-    # Store the toplevel group contents to the output svg.
-    svg_out.append(main_group)
-# Close the output svg.
-svg_out.append("</svg>")
+def write_svg(outfile,svg):
+    # Save svg to an output file.
+    if not outfile.endswith(".svg"):
+        print("Appended .svg to outfile")
+        outfile += ".svg"
+    with open(outfile,"w") as outfile:
+        outfile.write(svg)
+    return
 
-# Save output svg.
-outfile = args.outfile
-if not outfile.endswith(".svg"):
-    print("Appended .svg to outfile")
-    outfile += ".svg"
-with open(outfile,"w") as outfile:
-    outfile.write("\n".join(svg_out))
+if __name__=="__main__":
+    # Handle commandline args.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a","--alpha",default=45,type=float,help="alpha angle (angle counterclockwise about the z axis), in degrees")
+    parser.add_argument("-g","--gamma",default=np.degrees(np.arctan(np.sqrt(2))),type=float,help="gamma angle (angle counterclockwise about the x axis), in degrees")
+    parser.add_argument("-o","--outfile",default="out.svg",type=str,help="path to save projected diagram")
+    args = parser.parse_args()
+    alpha = np.radians(args.alpha)
+    gamma = np.radians(args.gamma)
+    outfile = args.outfile
+    svg = project_svg_collection(alpha,gamma,"x.svg","y.svg","z.svg")
+    write_svg(outfile,svg)
