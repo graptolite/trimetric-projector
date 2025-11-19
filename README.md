594e9db1a70355985a54bec0bf6db56cf2eb0497
graptolite <109924401+graptolite@users.noreply.github.com>
2025-11-19 01:09:38 +0000

    More doc.

---
+++ README.md
@@ -0,0 +1,3 @@
+Each face of the cuboid net (see Figure 1 of `derivation/derivation.pdf`) must go up to the page margins exactly (i.e., no rounded corners from boundary/edge paths, which should be put on afterwards). The SVG files must be named `x.svg`, `y.svg` and `z.svg` where the axis label (e.g., `x`) is the axis that the face is normal to.
+
+Tested for SVG files created and saved by Inkscape.
--- projector.py
+++ projector.py
@@ -1,3 +1,22 @@
+'''
+    Trimetric Projector | Project a collection of faces (in SVG format)
+ forming a cuboid net into trimetric/dimetric/isometric view
+    Copyright (C) 2025  Yingbo Li
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import numpy as np
 from numpy import cos,sin,tan
 import re
